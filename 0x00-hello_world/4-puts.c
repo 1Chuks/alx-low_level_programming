f35ab21b4fa599bceeaf37067e6c2457850abb28
Chukwuka <chukwuka.robert@gmail.com>
2022-10-13 15:16:25 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *  *  * main - Entry point
@@ -14,8 +13,8 @@ int main(void)
 
 {
 
-			puts("\"Programming is like building a multilingual puzzle");
+   puts("\"Programming is like building a multilingual puzzle");
 
-						return (0);
+       return (0);
 
 }
