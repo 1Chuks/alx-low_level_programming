12f502e94020da5b7be17baae67148e6840ced8d
Chukwuka <chukwuka.robert@gmail.com>
2022-10-19 00:06:51 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,35 @@
 #include "main.h"
+
+
+
 /**
- * main - print _putchar
- * is written by bwave
- * Return: 0
- */
+ *
+ *  * main - function to print
+ *
+ *   *
+ *
+ *    * Return: 0
+ *
+ *     */
 
 int main(void)
 
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+
+	char word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	unsigned int c;
+
+	for (c = 0; c < sizeof(word); c++)
+
+	{
+
+		_putchar(word[c]);
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+
 }
