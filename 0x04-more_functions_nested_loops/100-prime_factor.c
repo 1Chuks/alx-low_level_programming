808cc88b8b12f45410a61dbf44d5c76c1d88de34
Chukwuka <chukwuka.robert@gmail.com>
2022-10-21 14:08:55 -0700

    task 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+
+ 
+
+int largestprimefactor(unsigned long a)
+
+{
+
+	    int i =2 ,largeprimefactor = 2;
+
+	         
+
+	        while(a!=1)
+
+			    {
+
+				            if(a%i==0)
+
+						            {
+
+								                while(a%i==0)
+
+											            {
+
+													                    a = a/i;
+
+															                     
+
+															                    printf("%d ",i);
+
+																	                     
+
+																	                    if(i>largeprimefactor)
+
+																				                    {
+
+																							                        largeprimefactor = i;
+
+																										                }
+
+																			                }
+
+										        } 
+
+					             
+
+					            i++;
+
+						        }
+
+		     
+
+		    return largeprimefactor;
+
+}
+
+ 
+
+main()
+
+{
+
+	    unsigned long inputnumber;
+
+	         
+
+	        printf("Enter a number : ");
+
+		    scanf("%d",&inputnumber);
+
+		         
+
+		        printf("\nThe largest prime factor of %d is %d",inputnumber,largestprimefactor(inputnumber));
+
+}
