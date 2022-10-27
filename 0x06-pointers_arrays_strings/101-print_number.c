806b89bae04085897aecf82074e89069a15d7a5e
Chukwuka <chukwuka.robert@gmail.com>
2022-10-27 11:52:08 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,17 +4,21 @@
 
 /**
  *
- *  * print_number - Prints an integer.
+ *   * print_number - Prints any integer with putchar
  *
- *   * @n: The integer to be printed.
+ *     * @n: Number to prints
  *
- *    */
+ *       *
+ *
+ *         * Return: Nothing
+ *
+ *           */
 
 void print_number(int n)
 
 {
 
-		unsigned int num = n;
+		unsigned int x;
 
 
 
@@ -24,18 +28,22 @@ void print_number(int n)
 
 								_putchar('-');
 
-										num = -num;
+										n *= -1;
 
 											}
 
 
 
-				if ((num / 10) > 0)
-
-							print_number(num / 10);
+				x = n;
 
 
 
-					_putchar((num % 10) + '0');
+					if (x / 10)
+
+								print_number(x / 10);
+
+
+
+						_putchar(x % 10 + '0');
 
 }
