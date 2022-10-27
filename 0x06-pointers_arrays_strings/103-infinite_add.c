5f47ca00f761123995db17f91416fc58ad2ebb1d
Chukwuka <chukwuka.robert@gmail.com>
2022-10-27 12:42:07 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,11 +14,11 @@
  *
  *      * @size_r: buffer size
  *
- *       * Return: address of r or 0
+ *       * ahhh! Crazy task! Expand your knowledge
  *
- *        */
-
-
+ *        * Return: address of r or 0
+ *
+ *         */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
@@ -91,3 +91,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 											return (r);
 
 }
+
+/*Bwave */
